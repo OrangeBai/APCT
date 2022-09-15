254e2578a2feb53a336b145894eccdf7bd4b8b40
OrangeBai <jzp0306@gmail.com>
2022-09-15 18:39:57 +0100

    .

--- settings/train_settings.py
+++ settings/train_settings.py
@@ -94,7 +94,7 @@ class ArgParser:
             self.parser.add_argument('--gamma', default=0.2, type=float)
             self.parser.add_argument('--milestones', default=[0.3, 0.6, 0.8], nargs='+', type=float)  # for milestone
         elif args.lr_scheduler in ['exp', 'linear']:
-            self.parser.add_argument('--base_lr', default=0.0001 * args.lr)  # for linear
+            self.parser.add_argument('--base_lr', default=0.0001 * args.lr, type=float)  # for linear
         elif args.lr_scheduler == 'cyclic':
             self.parser.add_argument('--base_lr', default=0.0001 * args.lr)
             self.parser.add_argument('--up_ratio', default=1 / 3)
