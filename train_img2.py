e23739262f990ff7fb37bb753ae9b98485d94212
OrangeBai <jzp0306@gmail.com>
2022-09-18 17:13:42 +0100

    .

--- train_img2.py
+++ train_img2.py
@@ -9,8 +9,6 @@ os.environ['MASTER_PORT'] = '12355'
 if __name__ == '__main__':
     args = ArgParser(True).get_args()
 
-    # dist.init_process_group("nvcc", init_method=store, rank=rank, world_size=args.nnodes * args.nproc_per_node)
-    dist.init_process_group('gloo', init_method='tcp://127.0.0.1:28765',
-                            rank=args.local_rank, world_size=args.world_size)
+    dist.init_process_group('gloo', rank=args.local_rank, world_size=args.world_size)
     trainer = BaseTrainer(args, rank=args.local_rank)
     trainer.train_model()
