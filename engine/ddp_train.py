ffee74f3924ef338b1037605289b7eb1ad57b991
OrangeBai <jzp0306@gmail.com>
2022-09-18 18:00:09 +0100

    .

--- engine/ddp_train.py
+++ engine/ddp_train.py
@@ -2,7 +2,7 @@ import torch.distributed as dist
 from engine.trainer import BaseTrainer
 
 
-def train(args):
-    dist.init_process_group("gloo")
-    trainer = BaseTrainer(args, args.local_rank)
+def train(rank, args):
+    dist.init_process_group("gloo", rank=rank, world_size=args.world_size)
+    trainer = BaseTrainer(args, rank)
     trainer.train_model()
