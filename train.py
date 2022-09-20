03c7ae1928332d92a93b257a7cbafde7786c1577
steve <s.mander3@lancs.ac.uk>
2022-09-20 15:29:18 +0100

    update

--- train.py
+++ train.py
@@ -1,16 +1,10 @@
-from settings.train_settings import *
-from engine.trainer import BaseTrainer
-import torch.multiprocessing as mp
-import torch.distributed as dist
-from engine.ddp_train import train
+from settings.train_settings import ArgParser
+from engine.trainer import run
+
 os.environ['MASTER_ADDR'] = 'localhost'
 os.environ['MASTER_PORT'] = '12355'
 
 if __name__ == '__main__':
 
     args = ArgParser(True).get_args()
-
-    mp.spawn(train,
-             args=(args,),
-             nprocs=args.world_size,
-             join=True)
+    run(args)
