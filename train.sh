c0c23239169b1288d45fdbfc2b0eb3429dcc02fc
OrangeBai <jzp0306@gmail.com>
2022-09-22 12:14:03 +0000

    .

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 # python imagenet_to_hdf5.py --mode clean
 
-python train.py  --world_size 2 --dataset imagenet --attack noise --sigma 0.15 --exp_id noise_015 --resume 0 --print_every 10 --num_epoch 15
-# CUDA_VISIBLE_DEVICES=2,3 python train.py  --world_size 2 --dataset imagenet --exp_id normal  --resume 0 --print_every 10 --num_epoch 15 
+# python train.py  --world_size 2 --dataset imagenet --attack noise --sigma 0.15 --exp_id noise_015 --resume 0 --print_every 10 --num_epoch 15
+CUDA_VISIBLE_DEVICES=2,3 python train.py  --world_size 2 --dataset imagenet --exp_id normal  --resume 0 --print_every 10 --num_epoch 15 
 
 
 
