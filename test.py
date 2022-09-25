b04938bcafa8cb09555304f8d31e48f63753e9c2
OrangeBai <jzp0306@gmail.com>
2022-09-25 17:58:57 +0100

    .

--- test.py
+++ test.py
@@ -9,8 +9,7 @@ from exps.smoothed import *
 
 
 if __name__ == '__main__':
-    argsv = ['--dataset', 'cifar10', '--exp_id', 'noise_005', '--model_type', 'mini', '--test_name', 'smoothed_certify'
-             ,'--batch_size', '1000']
+    argsv = ['--dataset', 'cifar10', '--exp_id', 'noise_005', '--model_type', 'mini', '--test_name', 'smoothed_certify']
     torch.cuda.device_count()
     args = TestParser(argsv).get_args()
 
