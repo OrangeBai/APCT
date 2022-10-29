d40a4c747cff37083894ce0802ff1f85230ff3d8
OrangeBai <jzp0306@gmail.com>
2022-10-29 22:15:11 +0100

    .

--- engine/trainer.py
+++ engine/trainer.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from readline import set_pre_input_hook
+=======
+from tkinter.tix import Tree
+>>>>>>> 3078b1d (.)
 import torch.utils.data as data
 from pytorch_lightning.loggers import CSVLogger
 from attack import *
@@ -63,6 +67,7 @@ class PLModel(pl.LightningModule):
         outputs = self.model(images)
         loss = self.loss_function(outputs, labels)
         top1, top5 = accuracy(outputs, labels)
+<<<<<<< HEAD
         self.info = {'train/loss': loss, 'train/top1': top1, 'train/top5': top5[0], 'lr': self.optimizers().param_groups[0]['lr'], 'step': self.global_step}
         return loss
     
@@ -75,6 +80,55 @@ class PLModel(pl.LightningModule):
                 self.info['train/entropy/layer_var/{}'.format(str(i).zfill(2))] = r.var()
         
         wandb.log(self.info)
+=======
+        self.metrics.update(
+            top1=(top1, len(images)), top5=(top5, len(images)),
+            loss=(loss, len(images)),
+            lr=(self.get_lr(), 1)
+        )
+        self.metrics.all_reduce()
+
+    # def warmup(self):
+    #     if self.args.warmup_steps == 0:
+    #         return
+    #     loader = InfiniteLoader(self.train_loader)
+    #     self.lr_scheduler = warmup_scheduler(self.args, self.optimizer)
+    #     for cur_step in range(self.args.warmup_steps):
+    #         images, labels = next(loader)
+    #         images, labels = to_device(self.args.devices[0], images, labels)
+    #         # self.train_step(images, labels)
+    #         if cur_step % self.args.print_every == 0 and cur_step != 0 and self.rank == 0:
+    #             self.logger.step_logging(cur_step, self.args.warmup_steps, -1, -1, self.metrics, loader.metric)
+    #
+    #         if cur_step >= self.args.warmup_steps:
+    #             break
+    #     self.logger.train_logging(-1, self.args.num_epoch, self.metrics, loader.metric)
+    #     self.validate_epoch()
+    #     self.optimizer = init_optimizer(self.args, self.model)
+    #     self.lr_scheduler = init_scheduler(self.args, self.optimizer)
+    #
+    #     return
+
+    def train_epoch(self, epoch):
+        cur_time = time.time()
+        for step, (images, labels) in enumerate(self.train_loader):
+            data_time = time.time() - cur_time
+            images, labels = images.to(self.rank, non_blocking=True), labels.to(self.rank, non_blocking=True)
+            self.train_step(images, labels)
+            if step % self.args.print_every == 0 and step != 0 and self.rank == 0:
+                self.logger.step_logging(step, self.args.epoch_step, epoch, self.args.num_epoch,
+                                         self.metrics, self.time_metric)
+
+            iter_time = time.time() - cur_time
+
+            self.time_metric.update(iter_time=(iter_time, 1), data_time=(data_time, 1))
+            self.time_metric.all_reduce()
+            self.metrics.all_reduce()
+            cur_time = time.time()
+        if self.rank == 0:
+            self.logger.train_logging(epoch, self.args.num_epoch, self.metrics, self.time_metric)
+        self.time_metric.reset()
+>>>>>>> 3078b1d (.)
         return
 
 
@@ -96,6 +150,7 @@ class PLModel(pl.LightningModule):
             batch_output[key] = val
         return batch_output
 
+<<<<<<< HEAD
     def validation_epoch_end(self, validation_step_outputs) -> None:
         epoch_output = {}
         for out in validation_step_outputs:
@@ -112,6 +167,45 @@ class PLModel(pl.LightningModule):
         for i, r in enumerate(res):
             info['val/entropy_layer_{}'.format(str(i).zfill(2))] = list(r)
         wandb.log(info)
+=======
+        # self.warmup()
+
+        for epoch in range(self.start_epoch, self.args.num_epoch):
+            self.reset_lr_dt(epoch)
+            self.train_epoch(epoch)
+            self.record_result(epoch)
+
+            # acc = self.validate_epoch()
+            # if acc > self.best_acc:
+            #     self.best_acc = acc
+            #     if self.rank == 0:
+            #         self.save_ckpt(epoch + 1, self.best_acc, 'best')
+
+        if self.rank == 0:
+            if self.args.save_name == '':
+                self.args.save_name = 'epoch_{}'.format(str(self.args.num_epoch).zfill(3))
+            self.save_result(self.args.model_dir, self.args.save_name)
+            self.save_ckpt(self.args.num_epoch, self.best_acc, self.args.save_name)
+
+    def _init_dataset(self):
+        train_dataset, test_dataset = set_data_set(self.args)
+        self.train_sampler = DistributedSampler(train_dataset, shuffle=True)
+        self.test_sampler = DistributedSampler(test_dataset, shuffle=True)
+        self.train_loader = data.DataLoader(
+            dataset=train_dataset,
+            batch_size=self.args.batch_size,
+            sampler=self.train_sampler
+
+        )
+        self.test_loader = data.DataLoader(
+            dataset=test_dataset,
+            batch_size=self.args.batch_size,
+            sampler=self.test_sampler
+        )
+
+        self.args.epoch_step = len(self.train_loader)
+        self.args.total_step = self.args.num_epoch * self.args.epoch_step
+>>>>>>> 3078b1d (.)
         return
 
 
@@ -132,6 +226,94 @@ def run(args):
     logger=logtool,
     )
 
+<<<<<<< HEAD
     # datamodule=DataModule(args)
     model=PLModel(args)
     trainer.fit(model)
+=======
+    def save_result(self, path, name=None):
+        if not name:
+            res_path = os.path.join(path, 'result')
+        else:
+            res_path = os.path.join(path, 'result_{}'.format(name))
+        np.save(res_path, self.result)
+
+    def record_result(self, epoch, mode='train'):
+
+        epoch_result = {}
+        for k, v in self.metrics.meters.items():
+            epoch_result[k] = v.to_dict()
+        self.result[mode][epoch] = epoch_result
+        self.metrics.reset()
+        return
+
+    @property
+    def trained_ratio(self):
+        return self.lr_scheduler.last_epoch / self.args.total_step
+
+    def get_lr(self):
+        return self.optimizer.param_groups[0]['lr']
+
+    # def record_lip(self, images, labels, outputs):
+    #     perturbation = self.lip.attack(images, labels)
+    #     local_lip = (self.model(images + perturbation) - outputs)
+    #     lip_li = (local_lip.norm(p=float('inf'), dim=1) / perturbation.norm(p=float('inf'), dim=(1, 2, 3))).mean()
+    #     lip_l2 = (local_lip.norm(p=2, dim=1) / perturbation.norm(p=2, dim=(1, 2, 3))).mean()
+    #     self.update_metric(lip_li=(lip_li, len(images)), lip_l2=(lip_l2, len(images)))
+    #     return
+
+    def _init_functions(self):
+        self.optimizer = init_optimizer(self.args, self.model)
+        self.lr_scheduler = init_scheduler(self.args, self.optimizer)
+
+        self.loss_function = init_loss(self.args)
+
+    def reset_lr_dt(self, epoch):
+        if self.args.dataset != 'imagenet':
+            if epoch == 0:
+                self._init_dataset()
+                self._init_functions()
+            else:
+                return
+
+        if epoch == 0:
+
+            with open(self.args.phase_path, 'r') as f:
+                self.args.phase_file = yaml.load(f, Loader=yaml.FullLoader)
+            cur_p, cur_file = check_phase(self.args.phase_file, epoch)
+
+            self.args.data_size = cur_file['data_size']
+            self.args.crop_size = cur_file['crop_size']
+            self.args.batch_size = cur_file['batch_size']
+            self._init_dataset()
+
+            self.args.lr_scheduler = cur_file['lr_scheduler']
+            self.args.lr = cur_file['lr']
+            self.args.lr_e = cur_file['lr_e']
+            self.args.total_step = (cur_file['end_epoch'] - cur_file['start_epoch']) * len(self.train_loader)
+
+            self.logger.info('Switching to  {0}, with info {1}'.format(cur_p, cur_file))
+
+            self._init_functions()
+        else:
+            cur_p, cur_file = check_phase(self.args.phase_file, epoch)
+            pre_p, pre_file = check_phase(self.args.phase_file, epoch - 1)
+            if pre_p == cur_p:
+                return
+            self.logger.info('Switching to  {0}, with info {1}'.format(cur_p, cur_file))
+            if pre_file['data_size'] != cur_file['data_size']:
+                self.args.data_size = cur_file['data_size']
+                self.args.crop_size = cur_file['crop_size']
+                self.args.batch_size = cur_file['batch_size']
+                self._init_dataset()
+                self.logger.info('Dataset Initialized')
+
+            if cur_p != pre_p:
+                self.args.lr_scheduler = cur_file['lr_scheduler']
+                self.args.lr = cur_file['lr']
+                self.args.lr_e = cur_file['lr_e']
+                self.args.total_step = (cur_file['end_epoch'] - cur_file['start_epoch']) * len(self.train_loader)
+                self._init_functions()
+                self.logger.info('Optimizer Initialized')
+        return
+>>>>>>> 3078b1d (.)
