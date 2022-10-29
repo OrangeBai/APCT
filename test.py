ef2ea84f8a2a4f5134f163eb48ff16f4d4e5c50a
OrangeBai <jzp0306@gmail.com>
2022-10-29 22:29:12 +0100

    .

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from settings.test_setting import TestParser
 from models.base_model import build_model
 from exps.smoothed import *
@@ -21,17 +20,3 @@ if __name__ == '__main__':
 
 
 
-=======
-from PIL import Image
-
-path = '/home/orange/Main/Data/ImageNet/train/n04254680/n04254680_481.JPEG'
-im = Image.open(path)
-
-sz = 160
-w, h = im.size
-ratio = min(h / sz, w / sz)
-im = im.resize((int(w / ratio), int(h / ratio)), resample=Image.BICUBIC)
-
-new_fn = '/home/orange/Main/Data/test.JPEG'
-im.convert('RGB').save(new_fn)
->>>>>>> 8e9875c (.)
--- train.py
+++ train.py
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 from settings.train_settings import TrainParser
 from engine.trainer import run
 import wandb
-=======
-from settings.train_settings import *
-from engine.trainer import BaseTrainer
-import torch.multiprocessing as mp
-import torch.distributed as dist
-from engine.ddp_train import train
-os.environ['MASTER_ADDR'] = 'localhost'
-os.environ['MASTER_PORT'] = '12356'
->>>>>>> 3078b1d (.)
 
 if __name__ == '__main__':
     args = TrainParser().get_args()
